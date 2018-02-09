8c9a6206d2eb4d6dbe753e21d932a9bb60dca466
jimena ramirez <salmajiimenarmz@gmail.com>
2018-02-08 18:17:26 -0600

    Agregar columna

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208175942) do
+ActiveRecord::Schema.define(version: 20180209000703) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "photo"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
 end
